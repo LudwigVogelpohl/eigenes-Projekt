1e34bba020d8bcf663cad77a41d5beaeb25cab2d
LudwigVogelpohl <vogelpohlludwig@gmail.com>
2025-01-29 19:00:18 +0100

    script.js added

---
+++ script.js
@@ -0,0 +1,11 @@
+function newVokabulary(){
+    window.location.href = 'neue_Vokabel.html'
+}
+function zurück(){
+    window.location.href = 'index.html'
+}
+
+function secret(){
+    
+    window.location.href = 'index.html'
+}
